19e20799ee688a5eee4a387d8e8f33514a2c6639
Jane <49196776+jane13sh@users.noreply.github.com>
2019-06-18 11:42:09 +0300

    Update copyini.php

--- copyini.php
+++ copyini.php
@@ -1,4 +1,4 @@
 <?php
-        if (!copy('C:\Users\travis\build\eko-point\feeding-php-autotests\php.ini', 'C:\tools\php56\php.ini')) {
+        if (!copy('C:\Users\travis\build\jane13sh\ekotests_codeception\php.ini', 'C:\tools\php56\php.ini')) {
             echo "Не удалось скопировать $file...\n";    
 }
